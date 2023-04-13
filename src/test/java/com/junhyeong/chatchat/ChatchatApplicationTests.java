1840a4b2bcb05d6aadba033b3bc64a9887bc0444
junhyeong00 <jhbae0420@naver.com>
2023-04-13 23:23:50 +0900

    프로젝트 세팅

---
+++ src/test/java/com/junhyeong/chatchat/ChatchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junhyeong.chatchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

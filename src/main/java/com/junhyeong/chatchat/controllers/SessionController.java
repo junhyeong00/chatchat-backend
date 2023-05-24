ca66350c421266ddb52b7eb76c8a8b7191cac8ab
junhyeong00 <jhbae0420@naver.com>
2023-05-24 15:21:40 +0900

    Http-only Cookie를 사용하여 RefreshToken Rotation 기능 구현

--- src/main/java/com/junhyeong/chatchat/controllers/SessionController.java
+++ src/main/java/com/junhyeong/chatchat/controllers/SessionController.java
@@ -7,7 +7,9 @@ import com.junhyeong.chatchat.dtos.TokenDto;
 import com.junhyeong.chatchat.exceptions.LoginFailed;
 import com.junhyeong.chatchat.models.user.Password;
 import com.junhyeong.chatchat.models.user.UserName;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController
@@ -29,14 +32,34 @@ public class SessionController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public LoginResultDto login(
+            HttpServletResponse response,
             @Valid @RequestBody LoginRequestDto loginRequestDto
     ) {
+        try {
         UserName userName = UserName.of(loginRequestDto.userName());
         Password password = Password.of(loginRequestDto.password());
 
         TokenDto token = loginService.login(userName, password);
 
+            ResponseCookie cookie = ResponseCookie.from("refreshToken", token.refreshToken())
+                    .httpOnly(true)
+                    .path("/")
+                    .sameSite("Lax")
+                    .domain("localhost")
+                    .build();
+
+            response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
         return new LoginResultDto(token.accessToken());
+    } catch (Exception exception) {
+        throw new LoginFailed();
+    }
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String invalidRequest() {
+        return "이메일 혹은 비밀번호가 잘못 되었습니다";
     }
 
     @ExceptionHandler(LoginFailed.class)

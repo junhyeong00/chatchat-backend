1c55a409b0adaed7a7467049e50193edbe176b56
junhyeong00 <jhbae0420@naver.com>
2023-06-27 15:27:57 +0900

    고객 채팅 조회 기능 구현
    - GET /customer/chatrooms/{id}

--- src/test/java/com/junhyeong/chatchat/controllers/customer/CustomerChatRoomControllerTest.java
+++ src/test/java/com/junhyeong/chatchat/controllers/customer/CustomerChatRoomControllerTest.java
@@ -1,7 +1,11 @@
 package com.junhyeong.chatchat.controllers.customer;
 
+import com.junhyeong.chatchat.applications.chatRoom.GetCustomerChatRoomService;
 import com.junhyeong.chatchat.applications.chatRoom.GetCustomerChatRoomsService;
 import com.junhyeong.chatchat.dtos.ChatRoomDto;
+import com.junhyeong.chatchat.exceptions.ChatRoomNotFound;
+import com.junhyeong.chatchat.exceptions.CompanyNotFound;
+import com.junhyeong.chatchat.exceptions.CustomerNotFound;
 import com.junhyeong.chatchat.models.commom.Username;
 import com.junhyeong.chatchat.utils.JwtUtil;
 import org.junit.jupiter.api.Test;
@@ -27,6 +31,9 @@ class CustomerChatRoomControllerTest {
     @MockBean
     private GetCustomerChatRoomsService getChatRoomsService;
 
+    @MockBean
+    private GetCustomerChatRoomService getChatRoomService;
+
     @SpyBean
     private JwtUtil jwtUtil;
 
@@ -43,5 +50,62 @@ class CustomerChatRoomControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/customer/chatrooms")
                         .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void chatRoomDetail() throws Exception {
+        Username username = new Username("customer123");
+        String token = jwtUtil.encode(username);
+
+        Long chatRoomId = 1L;
+
+        mockMvc.perform(MockMvcRequestBuilders.get(String.format("/customer/chatrooms/%d", chatRoomId))
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void chatRoomDetailWithUnauthorized() throws Exception {
+        Username invalidUsername = new Username("xxx");
+        String token = jwtUtil.encode(invalidUsername);
+
+        Long chatRoomId = 999L;
+
+        given(getChatRoomService.chatRoomDetail(invalidUsername, chatRoomId))
+                .willThrow(ChatRoomNotFound.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(String.format("/customer/chatrooms/%d", chatRoomId))
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void chatRoomDetailWithChatRoomNotFound() throws Exception {
+        Username username = new Username("customer123");
+        String token = jwtUtil.encode(username);
+
+        Long chatRoomId = 999L;
+
+        given(getChatRoomService.chatRoomDetail(username, chatRoomId))
+                .willThrow(ChatRoomNotFound.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(String.format("/customer/chatrooms/%d", chatRoomId))
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void chatRoomDetailWithCompanyNotFound() throws Exception {
+        Username username = new Username("customer123");
+        String token = jwtUtil.encode(username);
+
+        Long chatRoomId = 1L;
+
+        given(getChatRoomService.chatRoomDetail(username, chatRoomId))
+                .willThrow(CompanyNotFound.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(String.format("/customer/chatrooms/%d", chatRoomId))
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isNotFound());
     }
 }

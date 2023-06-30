07079b92f8653ebbc317cff05a2374ac16b168e1
junhyeong00 <jhbae0420@naver.com>
2023-06-30 16:25:12 +0900

    자동 질문답변 추가 기능 구현
    - POST /auto-replies

--- src/main/java/com/junhyeong/chatchat/controllers/company/AutoReplyController.java
+++ src/main/java/com/junhyeong/chatchat/controllers/company/AutoReplyController.java
@@ -1,15 +1,23 @@
 package com.junhyeong.chatchat.controllers.company;
 
+import com.junhyeong.chatchat.applications.autoReply.CreateAutoReplyService;
 import com.junhyeong.chatchat.applications.autoReply.GetAutoRepliesService;
 import com.junhyeong.chatchat.dtos.AutoRepliesDto;
 import com.junhyeong.chatchat.dtos.AutoReplyDto;
+import com.junhyeong.chatchat.dtos.CreateAutoReplyRequest;
+import com.junhyeong.chatchat.dtos.CreateAutoReplyRequestDto;
+import com.junhyeong.chatchat.dtos.CreateAutoReplyResultDto;
+import com.junhyeong.chatchat.exceptions.CreateAutoReplyFailed;
 import com.junhyeong.chatchat.exceptions.Unauthorized;
 import com.junhyeong.chatchat.models.autoReply.AutoReply;
 import com.junhyeong.chatchat.models.commom.Username;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +28,12 @@ import java.util.List;
 @RequestMapping("auto-replies")
 public class AutoReplyController {
     private final GetAutoRepliesService getAutoRepliesService;
+    private final CreateAutoReplyService createAutoReplyService;
 
-    public AutoReplyController(GetAutoRepliesService getAutoRepliesService) {
+    public AutoReplyController(GetAutoRepliesService getAutoRepliesService,
+                               CreateAutoReplyService createAutoReplyService) {
         this.getAutoRepliesService = getAutoRepliesService;
+        this.createAutoReplyService = createAutoReplyService;
     }
 
     @GetMapping
@@ -36,9 +47,34 @@ public class AutoReplyController {
         return new AutoRepliesDto(autoReplyDtos);
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreateAutoReplyResultDto create(
+            @RequestAttribute Username username,
+            @Validated @RequestBody CreateAutoReplyRequestDto createAutoReplyRequestDto
+    ) {
+        try {
+            CreateAutoReplyRequest createAutoReplyRequest = CreateAutoReplyRequest.of(createAutoReplyRequestDto);
+
+            Long created = createAutoReplyService.create(username, createAutoReplyRequest);
+
+            return new CreateAutoReplyResultDto(created);
+        } catch (Unauthorized unauthorized) {
+            throw new Unauthorized();
+        } catch (Exception exception) {
+            throw new CreateAutoReplyFailed(exception.getMessage());
+        }
+    }
+
     @ExceptionHandler(Unauthorized.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public String unauthorized(Exception e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(CreateAutoReplyFailed.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String createAutoReplyFailed(Exception e) {
         return e.getMessage();
     }
 }
---
+++ src/main/java/com/junhyeong/chatchat/exceptions/CreateAutoReplyFailed.java
@@ -0,0 +1,7 @@
+package com.junhyeong.chatchat.exceptions;
+
+public class CreateAutoReplyFailed extends RuntimeException {
+    public CreateAutoReplyFailed(String message) {
+        super(message);
+    }
+}

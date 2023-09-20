b9cd1d155f6b8aec17fc515681c4e7882633c444
junhyeong00 <jhbae0420@naver.com>
2023-09-20 16:15:57 +0900

    웹소켓 인터셉터 변경 및 디버깅용 log 코드 추가

--- src/main/java/com/junhyeong/chatchat/interceptors/ChatWebSocketHandler.java
+++ src/main/java/com/junhyeong/chatchat/interceptors/ChatWebSocketHandler.java
@@ -13,7 +13,9 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ChatWebSocketHandler implements ChannelInterceptor {
     private static final Logger log = LoggerFactory.getLogger(MessageNotificationService.class);
     private SessionRepository sessionRepository;
